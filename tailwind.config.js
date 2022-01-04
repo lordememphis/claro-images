c0ac837b4dcbd06ba6091fb140c3421b8eacb25f
Memphis <muteket@gmail.com>
2022-01-04 17:32:26 +0200

    feat: add Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var  ", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#F16522",
+        accent: "#69BA10",
+      },
+    },
+  },
+  plugins: [],
+}
